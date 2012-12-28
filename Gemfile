5963458b97e545ee9d9cb8d3125408142a45f921
Todd Evanoff <todd@9mmedia.com>
2012-12-28 18:49:32 -0500

    First pass at capistrano deployment.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source :rubygems
 gem 'rspec'
 gem 'dino', git: 'https://github.com/djbwise/dino.git'
 gem 'rest-client'
+gem 'capistrano'
---
+++ config/deploy.rb
@@ -0,0 +1,36 @@
+require "bundler/capistrano"
+
+set :application, "charcutio"
+
+set :scm, :git
+set :repository,  "git@github.com:9mmedia/charcutio.git"
+set :deploy_to, "/usr/local/charcutio"
+ssh_options[:forward_agent] = true
+
+set :user, "pi"
+
+# for rbenv
+set :default_environment, {
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+}
+
+role :app, "10.10.10.157"
+
+namespace :deploy do
+  
+end
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
