eb25a0d82c40f5b7f8126d549120f155edf2f77b
Danielle Sucher <dsucher@gmail.com>
2013-01-02 19:37:39 -0500

    Add api_key to api client #post_meatshot

--- lib/fridge_api_client.rb
+++ lib/fridge_api_client.rb
@@ -19,10 +19,11 @@ class FridgeApiClient
   end
 
   def post_sensor_data(sensor_name, sensor_data)
-    RestClient.post "#{ENV['API_URL']}/boxes/#{@fridge.id}", api_key: ENV['API_KEY'], type: sensor_name, value: sensor_data
+    puts "trying to post sensor data (#{sensor_name}: #{sensor_data}"
+    RestClient.post "#{ENV['API_URL']}/boxes/#{@fridge.id}/report", api_key: ENV['API_KEY'], type: sensor_name, value: sensor_data
   end
 
   def post_meatshot(file_path)
-    RestClient.post "#{ENV['API_URL']}/boxes/#{@fridge.id}/photo", image_file: File.new(file_path, 'rb')
+    RestClient.post "#{ENV['API_URL']}/boxes/#{@fridge.id}/photo", api_key: ENV['API_KEY'], image_file: File.new(file_path, 'rb')
   end
 end
