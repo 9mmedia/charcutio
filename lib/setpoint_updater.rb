b75b519f6d1493ee238a8e6f75d81887d604c221
Danielle Sucher <dsucher@gmail.com>
2013-01-10 15:42:54 -0500

    Change temp humidity setpoint to 70

--- lib/setpoint_updater.rb
+++ lib/setpoint_updater.rb
@@ -9,7 +9,7 @@ class SetpointUpdater
   private
 
     def update_set_points
-      Celluloid::Actor[:humidistat].goal_state = 50 if Celluloid::Actor[:humidistat]
+      Celluloid::Actor[:humidistat].goal_state = 70 if Celluloid::Actor[:humidistat]
       Celluloid::Actor[:thermostat].goal_state = 13 if Celluloid::Actor[:thermostat]
       # set_points = FridgeApiClient.get_set_points
       # Celluloid::Actor[:humidistat].goal_state = set_points['humidity'].to_f
