152f6fc54048d1d1b77a045fb88eaec522ae8fd6
Danielle Sucher <dsucher@gmail.com>
2013-01-10 21:10:32 -0500

    Randomize light color on door open when possible

--- lib/photo/light.rb
+++ lib/photo/light.rb
@@ -1,11 +1,12 @@
-
 class Light
 
-  def initialize(charcutio)
-    @charcutio = charcutio
-    @board = charcutio.board
-    @pins = charcutio.pins[:light_pins].split(",").map(&:strip)
-    @lights = @pins.map { |pin| Dino::Components::Led.new(pin: pin, board: @board) }
+  def initialize(fridge)
+    pins = fridge.pins[:light_pins].split(",").map(&:strip)
+    if pins.count == 3
+      @rgb_light = Dino::Components::RgbLed.new board: fridge.board, pins: {red: pins[0], green: pins[1], blue: pins[2]}
+    else
+      @lights = pins.map { |pin| Dino::Components::Led.new(pin: pin, board: fridge.board) }
+    end
     @state = :off
   end
 
@@ -18,14 +19,38 @@ class Light
   end
 
   def on
-    @lights.each { |l| l.send(:on) }
+    @lights.each { |l| l.send(:on) } if @lights
+    randomize_rgb_light_on if @rgb_light
     @state = :on
     self
   end
 
   def off
-    @lights.each { |l| l.send(:off) }
+    @lights.each { |l| l.send(:off) } if @lights
+    rgb_light_off if @rgb_light
     @state = :off
     self
   end
+
+  private
+
+    def random_rgb_states
+      states = (0..2).map { |i| [Dino::Board::LOW, Dino::Board::HIGH].sample }
+      # make sure to turn at least one pin on when turning the rgb_light
+      states[0] = Dino::Board::HIGH unless states.include? Dino::Board::HIGH
+      states.shuffle!
+    end
+
+    def randomize_rgb_light_on
+      states = random_rgb_states
+      @rgb_light.send :analog_write, states[0], @rgb_light.pins[:red]
+      @rgb_light.send :analog_write, states[1], @rgb_light.pins[:green]
+      @rgb_light.send :analog_write, states[2], @rgb_light.pins[:blue]
+    end
+
+    def rgb_light_off
+      @rgb_light.send :analog_write, Dino::Board::LOW, @rgb_light.pins[:red]
+      @rgb_light.send :analog_write, Dino::Board::LOW, @rgb_light.pins[:green]
+      @rgb_light.send :analog_write, Dino::Board::LOW, @rgb_light.pins[:blue]
+    end
 end
