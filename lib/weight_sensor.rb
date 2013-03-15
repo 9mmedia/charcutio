6c23a94616a53ebb214f48515d17b0b77f4882d1
Brian Weisenthal <djbwise@bws-MacBook-Pro.local>
2013-03-15 17:04:04 -0400

    trying some averaging

--- lib/weight_sensor.rb
+++ lib/weight_sensor.rb
@@ -1,17 +1,27 @@
 class WeightSensor
   include Celluloid
 
+  @data = []
+
   def initialize
     every(30) { post_latest_sensor_data }
   end
 
   def latest_sensor_data=(value)
     @latest_sensor_data = value.to_f
+    @data.push(@latest_sensor_data)
   end
 
   private
 
     def post_latest_sensor_data
+      @data.sort!
+      start = (@data.size * 0.25).to_i
+      last = (@data.size * .75).to_i
+      trimmed_data = @data[start..last]
+      avg = trimmed_data.reduce(:+).to_f / trimmed_data.size
+      puts avg
       FridgeApiClient.post_data_point 'weight', @latest_sensor_data if @latest_sensor_data
+      @data = []
     end
 end
